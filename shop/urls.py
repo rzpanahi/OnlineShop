70f1d618bea69bcc9d0498bb48325c6d1dbc9803
Reza Panahi <rezapanahiads2@gmail.com>
2024-12-03 04:44:23 +0330

    add product page

--- shop/urls.py
+++ shop/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index, store
+from .views import index, store, product
 
 urlpatterns = [
     path('index/', index, name='index'),
     path('store/', store, name='store'),
+    path('product/', product, name='product'),
 ]
