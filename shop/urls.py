698e082df47479056d438a377c77776829b64ab3
Reza Panahi <rezapanahiads2@gmail.com>
2024-12-03 04:47:15 +0330

    add checkout page

--- shop/urls.py
+++ shop/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, store, product
+from .views import index, store, product, checkout
 
 urlpatterns = [
     path('index/', index, name='index'),
     path('store/', store, name='store'),
     path('product/', product, name='product'),
+    path('checkout/', checkout, name='checkout'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -9,3 +9,6 @@ def store(request):
 
 def product(request):
     return render(request, 'product.html')
+
+def checkout(request):
+    return render(request, 'checkout.html')
