b664ba6a392cfdcd636a13ad59332fb6aed8a4a1
Reza Panahi <rezapanahiads2@gmail.com>
2025-07-21 20:44:58 +0330

    fix delete action in all model admins

--- shop/admin.py
+++ shop/admin.py
@@ -1,14 +1,23 @@
 from django.contrib import admin
 from . import models
 
-admin.site.register(models.Category)
-admin.site.register(models.Product)
-admin.site.register(models.Cart)
-# admin.site.register(models.CartItem)
-admin.site.register(models.Order)
-admin.site.register(models.OrderItem)
-admin.site.register(models.Payment)
 
+class CategoryAdmin(admin.ModelAdmin):
+    def delete_queryset(self, request, queryset):
+        for category in queryset:
+            category.delete()
+
+
+class ProductAdmin(admin.ModelAdmin):
+    def delete_queryset(self, request, queryset):
+        for product in queryset:
+            product.delete()
+
+
+class CartAdmin(admin.ModelAdmin):
+    def delete_queryset(self, request, queryset):
+        for cart in queryset:
+            cart.delete()
 
 
 class CartItemAdmin(admin.ModelAdmin):
@@ -16,4 +25,22 @@ class CartItemAdmin(admin.ModelAdmin):
         for cartItem in queryset:
             cartItem.delete()
 
+
+class OrderAdmin(admin.ModelAdmin):
+    def delete_queryset(self, request, queryset):
+        for order in queryset:
+            order.delete()
+
+
+class OrderItemAdmin(admin.ModelAdmin):
+    def delete_queryset(self, request, queryset):
+        for orderItem in queryset:
+            orderItem.delete()
+
+
+admin.site.register(models.Category, CategoryAdmin)
+admin.site.register(models.Product, ProductAdmin)
+admin.site.register(models.Cart, CartAdmin)
 admin.site.register(models.CartItem, CartItemAdmin)
+admin.site.register(models.Order, OrderAdmin)
+admin.site.register(models.OrderItem, OrderItemAdmin)
